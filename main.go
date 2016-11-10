8a385703cd49b499a238a93014f07d5639dfc59b
OmarAYousry <omarahm3d19@gmail.com>
2016-11-10 21:58:27 +0200

    Making some changes

--- main.go
+++ main.go
@@ -4,25 +4,26 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/ramin0/chatbot"
+
+	// Autoload environment variables in .env
+	_ "github.com/joho/godotenv/autoload"
 )
 
-// Autoload environment variables in .env
-import _ "github.com/joho/godotenv/autoload"
+func chatbotProcess(session chatbot.Session, message string) (string, error) {
+	if strings.EqualFold(message, "chatbot") {
+		return "", fmt.Errorf("This can't be, I'm the one and only %s!", message)
+	}
 
-// func chatbotProcess(session chatbot.Session, message string) (string, error) {
-// 	if strings.EqualFold(message, "chatbot") {
-// 		return "", fmt.Errorf("This can't be, I'm the one and only %s!", message)
-// 	}
-
-// 	return fmt.Sprintf("Hello %s, my name is chatbot. What was yours again?", message), nil
-// }
+	return fmt.Sprintf("Hello %s, my name is chatbot. What was yours again?", message), nil
+}
 
 func main() {
 	// Uncomment the following lines to customize the chatbot
-	// chatbot.WelcomeMessage = "What's your name?"
-	// chatbot.ProcessFunc(chatbotProcess)
+	chatbot.WelcomeMessage = "What's your name?"
+	chatbot.ProcessFunc(chatbotProcess)
 
 	// Use the PORT environment variable
 	port := os.Getenv("PORT")
